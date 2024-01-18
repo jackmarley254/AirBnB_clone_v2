d05e7d812da89455901b7904a7ee5c87c379db80
Lamber22 <Lamberjoe5@gmail.com>
2024-01-18 15:02:52 -0800

    update

--- console.py
+++ console.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ Console Module """
 import cmd
+import re
 import sys
+from shlex import split
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
